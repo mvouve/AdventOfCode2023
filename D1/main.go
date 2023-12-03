ed61c60e90124e720da07dab7f59e23bcb4b84a4
Marc Vouve <mvouve@gmail.com>
2023-12-02 16:14:23 -0800

    Added some basic comments

--- D1/main.go
+++ D1/main.go
@@ -5,20 +5,21 @@ import (
 	"os"
 	"strconv"
 	"strings"
-
 )
 
+// sub inserts the numerical letter into the written letter so it can be picked up
+// without missing the possibility of words like two and one being merged (e.g. twone)
 func sub(s string) string {
 	for o, r := range map[string]string{
-		"one": "on1e",
-		"two": "tw2o",
+		"one":   "on1e",
+		"two":   "tw2o",
 		"three": "thr3ee",
-		"four": "fo4ur",
-		"five": "fi5ve",
-		"six": "s6ix",
+		"four":  "fo4ur",
+		"five":  "fi5ve",
+		"six":   "s6ix",
 		"seven": "se7ven",
 		"eight": "ei8ght",
-		"nine": "ni9ne",
+		"nine":  "ni9ne",
 	} {
 		s = strings.ReplaceAll(s, o, r)
 	}
@@ -26,6 +27,7 @@ func sub(s string) string {
 	return s
 }
 
+// panic and exit if error detected.
 func ferror(err error) {
 	if err != nil {
 		panic(err)
@@ -39,28 +41,27 @@ func main() {
 	lines := strings.Split(string(dat), "\n")
 
 	var total int64
-	for n, r := range lines {
+	for _, r := range lines {
 		l := sub(r)
-		cal := ""
+		cal := make([]byte, 2)
+		// start from start of sting to get first digit.
 		for idx := 0; idx < len(l); idx++ {
 			if l[idx] >= '0' && l[idx] <= '9' {
-				cal = string(append([]byte(cal), l[idx]))
+				cal[0] = l[idx]
 				break
 			}
 		}
 
 		for idx := len(l) - 1; idx >= 0; idx-- {
 			if l[idx] >= '0' && l[idx] <= '9' {
-				cal = string(append([]byte(cal), l[idx]))
+				cal[1] = l[idx]
 
 				break
 			}
 		}
-		t, _ := strconv.Atoi(cal)
-		fmt.Println(n, l, t)
+		t, _ := strconv.Atoi(string(cal))
 		total += int64(t)
 	}
 
 	fmt.Println(total)
 }
-
